19326e27c9c15f7cc99abbde8284757aec546b71
dannylmathews <dannyl.mathews@gmail.com>
2015-01-07 10:33:21 -0800

    Initial Commit

---
+++ MDTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDTest
+//
+//  Created by Daniel Mathews on 2015-01-07.
+//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MDTest/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  MDTest
+//
+//  Created by Daniel Mathews on 2015-01-07.
+//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
